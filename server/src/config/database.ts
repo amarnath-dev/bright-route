ca3d63ea0bf10284ef02d29d5fced4fa5d0cc20a
Amarnath AS <amarnathas2003@gmail.com>
2024-01-21 21:02:44 +0530

    ts-node installation fix

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,7 +1,7 @@
 import app from "../app";
 import mongoose from "mongoose";
 
-const PORT = 3000;
+const PORT = 5000;
 
 mongoose.connect("mongodb+srv://amarnathas:ChpaJ5TS9NRtWYvM@cluster0.u1pctnk.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log("Connected to Database Successfully");
