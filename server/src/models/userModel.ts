fd2d46eee3f03fc8ff65adab31ce5b22c041ae9f
Amarnath AS <amarnathas2003@gmail.com>
2024-01-23 14:40:20 +0530

    feat: Mentee signup sign in with otp verification functionality

---
+++ server/src/models/userModel.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "../Interfaces";
+
+const userSchema: Schema<IUser> = new mongoose.Schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+    is_blocked: {
+        type: Boolean,
+        default: false,
+    }
+}, { timestamps: true });
+
+export default mongoose.model<IUser>("User", userSchema);
