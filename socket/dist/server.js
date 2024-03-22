77f367a8095afb1e5fd1f4a96843f15e45b06e66
Amarnath AS <amarnathas2003@gmail.com>
2024-03-22 23:27:37 +0530

    fix: socket.io build change

---
+++ socket/dist/server.js
@@ -0,0 +1,58 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const socket_io_1 = require("socket.io");
+const io = new socket_io_1.Server({
+    cors: {
+        origin: "https://bright-route.online",
+        credentials: true,
+    },
+});
+let users = [];
+const addUser = (userId, socketId) => {
+    if (!users.some((user) => user.userId === userId)) {
+        users.push({ userId, socketId });
+    }
+};
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
+io.on("connection", (socket) => {
+    socket.on("addUser", (userId) => {
+        console.log("User Added");
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
+    socket.on("sendMessage", (message) => {
+        const user = getUser(message === null || message === void 0 ? void 0 : message.receiverId);
+        if (user && message) {
+            const { socketId } = user;
+            io.to(socketId).emit("getMessage", message);
+        }
+        else {
+            console.error(`User with ID ${message === null || message === void 0 ? void 0 : message.receiverId} not found`);
+        }
+    });
+    socket.on("sendNotification", ({ senderId, receiverId, content, type }) => {
+        const user = getUser(receiverId);
+        if (user) {
+            const { socketId } = user;
+            io.to(socketId).emit("getNotification", {
+                senderId,
+                content,
+                type,
+            });
+        }
+        else {
+            console.error(`User with ID ${receiverId} not found`);
+        }
+    });
+    socket.on("disconnect", () => {
+        removeUser(socket.id);
+        console.log("User removed");
+        io.emit("getUsers", users);
+    });
+});
+io.listen(3000);
