f91d985f7bc41ee98b4c5bb902e1aef9d58ee190
Amarnath AS <amarnathas2003@gmail.com>
2024-04-09 09:42:28 +0530

    fix: production build

--- client/src/app/useAxiosPrivate.ts
+++ client/src/app/useAxiosPrivate.ts
@@ -39,6 +39,7 @@ const useAxiosPrivate = () => {
       axiosPrivate.interceptors.request.eject(requestIntercept);
       axiosPrivate.interceptors.response.eject(responseIntercept);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accessToken, refresh]);
   return axiosPrivate;
 };
