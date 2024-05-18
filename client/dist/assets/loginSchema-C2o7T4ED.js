74a43fa08434e8ad210114539bcdf921a7375e4c
Amarnath AS <amarnathas2003@gmail.com>
2024-05-18 12:08:58 +0530

    fix: password info given

---
+++ client/dist/assets/loginSchema-C2o7T4ED.js
@@ -0,0 +1,1 @@
+import{z as s}from"./index-C9YQ_DTW.js";const o=s.object({email:s.string().email().refine(e=>e.trim()!=="",{message:"Email should not be empty"}),password:s.string().refine(e=>e.trim()!=="",{message:"Password should not be empty"})});export{o as l};
